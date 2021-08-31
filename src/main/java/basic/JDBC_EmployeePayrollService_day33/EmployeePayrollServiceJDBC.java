8e26e8faaf2e90213ba4d33555b27323ed4802ae
moshtab <mdmohsin41439@gmail.com>
2021-08-31 17:25:53 +0530

    added msg

---
+++ src/main/java/basic/JDBC_EmployeePayrollService_day33/EmployeePayrollServiceJDBC.java
@@ -0,0 +1,8 @@
+package basic.JDBC_EmployeePayrollService_day33;
+
+public class EmployeePayrollServiceJDBC {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JDBC connectivity payroll_service program");
+	}
+}
